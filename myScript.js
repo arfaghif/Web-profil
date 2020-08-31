93f0645288166a9e98a31fb89b2adc150e2898f2
arfaghif <52845090+arfaghif@users.noreply.github.com>
2020-08-31 15:49:43 +0700

    add alert

--- myScript.js
+++ myScript.js
@@ -3,8 +3,14 @@ function openInNewTab(url) {
     window.open(url, '_blank').focus();
 }
 function showObject(id){
+    //Menampilkan obyek HTML
     document.getElementById('request').style.display ='block';
 }
 function hideObject(id){
+    //Menyembunyikan obyek HTML
     document.getElementById('requestbutton').style.display ='none';
+}
+function myAlert(msg){
+    //Menampilkan window Alert
+    window.alert(msg);
 }
